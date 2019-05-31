6baa38e02c9f30f0b0b178ed31dbfaf74b5c2aee
viniciusfcar <38993929+viniciusfcar@users.noreply.github.com>
2019-05-30 21:45:58 -0300

    Update EditoraController.java

--- src/main/java/com/livraria/Livraria/controller/EditoraController.java
+++ src/main/java/com/livraria/Livraria/controller/EditoraController.java
@@ -21,34 +21,34 @@ public class EditoraController {
 	@Autowired
 	private EditoraService service;
 	
-	@GetMapping("lista_editoras")
+	@GetMapping("/lista_editoras")
 	public ModelAndView listaEditoras() {
 		ModelAndView mv = new ModelAndView("editora/lista_editoras");
 		mv.addObject("editoras", service.findAll());
 		return mv;
 	}
 	
-	@PostMapping("salvar_editora")
+	@PostMapping("/salvar_editora")
 	public ModelAndView save(Editora editora) {
 		
 		service.save(editora);
 		return listaEditoras();
 	}
 	
-	@RequestMapping("cadastro_editora")
+	@RequestMapping("/cadastro_editora")
 	public ModelAndView cadastroEditora(Editora editora) {
 		ModelAndView mv = new ModelAndView("editora/cadastro_editora");
 		mv.addObject("editora", editora);
 		return mv;
 	}
 	
-	@GetMapping("delete_editora/{id}")
+	@GetMapping("/delete_editora/{id}")
 	public ModelAndView deleteEditora(@PathVariable("id") long id) {
 		service.delete(id);
 		return listaEditoras();
 	}
 	
-	@GetMapping("alterar_editora/{id}")
+	@GetMapping("/alterar_editora/{id}")
 	public ModelAndView update(@PathVariable("id") long id) {
 		Editora editora = service.findOne(id);
 		return cadastroEditora(editora);
